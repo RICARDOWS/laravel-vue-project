b01896b41498c3ae6f9ff7283061a6d57cf94b14
Ricardo Bebeto Jimenez Garcia <bbto_j_g@hotmail.com>
2023-01-15 14:21:51 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <RouterView />
+    <Footer/>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<style scoped>
+</style>
+ 
---
+++ src/components/BaseModal.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Vista
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+</script>
